157fbfce873c5e961dca34a5aaa8b5f1bc38ebf6
招文桃 <gdouzwt@gmail.com>
2019-11-27 01:25:01 +0800

    so common

--- src/main/kotlin/io/zwt/reservationclient/ReservationClientApplication.kt
+++ src/main/kotlin/io/zwt/reservationclient/ReservationClientApplication.kt
@@ -1,19 +1,14 @@
 package io.zwt.reservationclient
 
-import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
 import org.springframework.cloud.gateway.filter.ratelimit.PrincipalNameKeyResolver
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter
-import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpHeaders
-import reactor.core.publisher.Mono
-import java.security.Principal
 
 @SpringBootApplication
 class ReservationClientApplication {
@@ -30,9 +25,7 @@ class ReservationClientApplication {
                 setPath("/reservations")
                 addRequestHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
                 requestRateLimiter {
-
                     it.rateLimiter = redisRateLimiter()
-                    it.keyResolver = PrincipalNameKeyResolver()
                 }
             }
             uri("http://localhost:8080")
