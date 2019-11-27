c0483a1b20eb7c15637c6586d319e3490007e261
招文桃 <gdouzwt@gmail.com>
2019-11-27 20:21:29 +0800

    update

--- src/main/kotlin/io/zwt/reservationclient/ReservationClientApplication.kt
+++ src/main/kotlin/io/zwt/reservationclient/ReservationClientApplication.kt
@@ -9,13 +9,62 @@ import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.support.beans
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.messaging.rsocket.RSocketRequester
+import org.springframework.messaging.rsocket.retrieveFlux
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.security.core.userdetails.User
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
+import java.time.Duration
+
+
+data class Reservation(val id: Integer, val name: String)
+data class GreetingRequest(val name: String)
+data class GreetingResponse(val message: String)
 
 @SpringBootApplication
 class ReservationClientApplication {
+
+    @Bean
+    fun webClient(builder: WebClient.Builder) = builder.build()
+
+    @Bean
+    fun rsocketClient(rsocketBuilder: RSocketRequester.Builder) = rsocketBuilder.connectTcp("localhost", 7777).block()
+
+    @Bean
+    fun route(webClient: WebClient, rsocketClient: RSocketRequester) = router {
+
+        GET("greetings/{name}") {
+            val request = GreetingRequest(it.pathVariable("name"))
+            val greetings = rsocketClient.route("greetings").data(request).retrieveFlux<GreetingResponse>()
+            ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(greetings)
+        }
+
+
+        GET("/reservations/names") {
+
+            //            val call1: Flux<String>? = null // todo
+//            val call2: Flux<String>? = null // todo
+//            val call3: Flux<String>? = null // todo
+//            val first: Flux<String> = Flux.first(call1, call2, call3)
+
+            val reservations = webClient
+                    .get()
+                    .uri("http://localhost:8080/reservations").retrieve().bodyToFlux<Reservation>()
+                    .map { it.name }
+                    .retryBackoff(10, Duration.ofSeconds(1))
+//                    .timeout(Duration.ofSeconds(1))
+//                    .onErrorResume { Flux.empty() }
+            ServerResponse.ok().body(reservations)
+        }
+    }
 
     @Bean
     fun authorization(http: ServerHttpSecurity) =
@@ -58,7 +107,7 @@ fun main(args: Array<String>) {
     runApplication<ReservationClientApplication>(*args) {
 
         val context = beans {
-//            if (Math.random() > .5)
+            //            if (Math.random() > .5)
             bean {
                 RedisRateLimiter(5, 7)
             }
